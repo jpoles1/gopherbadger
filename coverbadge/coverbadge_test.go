567925ff1e8172aa4a53570817e75a606781f52e
Marc Brugger <github@bakito.ch>
2020-08-20 08:11:57 +0200

    fix url rendering of singe decimal coverage (#16)
    Fixes invalid URLs when decimals are included in the coverage value, thanks to @bakito!

--- coverbadge/coverbadge_test.go
+++ coverbadge/coverbadge_test.go
@@ -15,3 +15,18 @@ func TestDownloadBadge(t *testing.T) {
 func TestWriteBadgeToMd(t *testing.T) {
 	coverageBadge.WriteBadgeToMd("coverage_test.md", 22)
 }
+func TestGenerateBadgeBadgeURL_1(t *testing.T) {
+	testUrl(t, 1.1, "https://img.shields.io/badge/Go%20Coverage-1%25-brightgreen.png?longCache=true&style=flat")
+}
+func TestGenerateBadgeBadgeURL_10(t *testing.T) {
+	testUrl(t, 10.1, "https://img.shields.io/badge/Go%20Coverage-10%25-brightgreen.png?longCache=true&style=flat")
+}
+func TestGenerateBadgeBadgeURL_100(t *testing.T) {
+	testUrl(t, 100.1, "https://img.shields.io/badge/Go%20Coverage-100%25-brightgreen.png?longCache=true&style=flat")
+}
+func testUrl(t *testing.T, coverageFloat float64, expected string) {
+	url := coverageBadge.generateBadgeBadgeURL(coverageFloat)
+	if url != expected {
+		t.Fatal("url should be", expected, "but is", url)
+	}
+}
