030ae578cf645f1d20237c90b0855ddf0526e58f
Michael Jarvis <michael@jarvis.com>
2021-08-19 07:07:00 -0500

    Make tests consistent, add comment
    The "success" conditions should always succeed, unless there is some
    regression bug that creeps in.

--- main_test.go
+++ main_test.go
@@ -25,19 +25,22 @@ func TestBadger(t *testing.T) {
 }
 
 func TestDrawBadge(t *testing.T) {
+	// Test success conditions
 	if err := drawBadge(22.7, "test_badge.png"); err != nil {
-		t.Errorf("error drawing 22.7 coverage: %s", err.Error())
+		t.Errorf("error drawing 22.7%% coverage: %s", err.Error())
 	}
 	if err := drawBadge(88, "test_badge.png"); err != nil {
-		t.Errorf("error drawing 88% coverage: %s", err.Error())
+		t.Errorf("error drawing 88%% coverage: %s", err.Error())
 	}
 	if err := drawBadge(66, "test_badge.png"); err != nil {
-		t.Errorf("error drawing 66% coverage: %s", err.Error())
+		t.Errorf("error drawing 66%% coverage: %s", err.Error())
 	}
-	if drawBadge(66, "bad_folder/test_badge.png") == nil {
-		t.Error("Should respond with error when saving to invalid folder")
+
+	// Test failure conditions
+	if err := drawBadge(66, "bad_folder/test_badge.png"); err == nil {
+		t.Errorf("should respond with error when saving to invalid folder")
 	}
-	if drawBadge(-34, "test_badge.png") == nil {
-		t.Error("Should respond with error when coverage is less than 0")
+	if err := drawBadge(-34, "test_badge.png"); err == nil {
+		t.Errorf("should respond with error when coverage is less than 0")
 	}
 }
